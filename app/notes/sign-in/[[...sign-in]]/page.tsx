ec84c8c7ee89b6929aa24e0a376f0eb82b83295d
Ilyas Seisov <ilyasseisov@gmail.com>
2025-06-02 13:27:35 +0500

    feat: add after-signin API route and update SignIn component with redirect URL

--- app/notes/sign-in/[[...sign-in]]/page.tsx
+++ app/notes/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,12 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
+  // local vars
+  const redirectUrl = `/api/auth/after-signin`;
+  // return
   return (
     <div className="fixed inset-0 flex items-center justify-center overflow-hidden">
-      <SignIn />
+      <SignIn forceRedirectUrl={redirectUrl} />
     </div>
   );
 }
