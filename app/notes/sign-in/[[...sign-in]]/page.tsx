65ee80f04504c1ed9c13d09bb8b0554a620d8e95
Ilyas Seisov <ilyasseisov@gmail.com>
2025-06-04 12:22:57 +0500

    feat: update SignIn component to remove redirect URL and enhance user flow

--- app/notes/sign-in/[[...sign-in]]/page.tsx
+++ app/notes/sign-in/[[...sign-in]]/page.tsx
@@ -2,11 +2,15 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   // local vars
-  const redirectUrl = `/api/auth/after-signin`;
   // return
   return (
     <div className="fixed inset-0 flex items-center justify-center overflow-hidden">
-      <SignIn forceRedirectUrl={redirectUrl} />
+      <SignIn
+        path="/notes/sign-in"
+        signUpUrl="/notes/sign-up"
+        routing="path"
+        oauthFlow="redirect"
+      />
     </div>
   );
 }
--- to-do.md
+++ to-do.md
@@ -1,10 +1,11 @@
+0. sign-in flow
+
 1. think flow for cancelling subscription
 2. think flow for auto-renew subscription
 3. free account (max 10 notes)
-4. sign-in flow
-5. 404
-6. add AI
-7. stylize sign-in/up forms as Shadcn
+4. 404
+5. add AI
+6. stylize sign-in/up forms as Shadcn
    https://clerk.com/docs/customization/elements/examples/shadcn-ui?utm_source=youtube&utm_medium=social&utm_campaign=elements-video
 
 ---
