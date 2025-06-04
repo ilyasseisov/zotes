744d13e004b0d342a86a4ac88033e34eaa7f5c64
Ilyas Seisov <ilyasseisov@gmail.com>
2025-06-04 12:27:01 +0500

    feat: add signInUrl prop to SignUp component for improved navigation

--- app/notes/sign-up/[[...sign-up]]/page.tsx
+++ app/notes/sign-up/[[...sign-up]]/page.tsx
@@ -20,6 +20,7 @@ export default function Page() {
           unsafeMetadata={{
             selectedPlan: plan,
           }}
+          signInUrl="/notes/sign-in"
         />
       </div>
     </div>
