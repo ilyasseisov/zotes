1dd1cff7f8e360930145745b226d34ecb3f36be7
Ilyas Seisov <ilyasseisov@gmail.com>
2025-06-23 13:00:59 +0500

    fix: change TypeScript ESLint rule for no-explicit-any to off

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn", // Show warnings instead of errors
+      "@typescript-eslint/no-explicit-any": "off", // Show warnings instead of errors
     },
   },
 ];
