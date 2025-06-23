62e56595b57b09614d614a1a5d2bae3dd9583761
Ilyas Seisov <ilyasseisov@gmail.com>
2025-06-23 12:54:57 +0500

    fix: remove TypeScript rule disabling for no-explicit-any in configuration

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off", // Turns off the rule
-    // Or to warn instead of error:
-    // "@typescript-eslint/no-explicit-any": "warn",
-  },
 };
 
 export default nextConfig;
